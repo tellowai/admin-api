da509be1ae468d311b5629a1e3aa1bfee26d48f2
Bhargav Dath <bhargavdath.chityala@gmail.com>
2025-01-15 21:29:58 +0530

    update all.js

--- config/env/all.js
+++ config/env/all.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   app: {
-		title: 'Photobop app API',
-		description: 'API for Photobop app'
+		title: 'Photobop admin API',
+		description: 'Admin API for Photobop app'
 	},
 	port: 5000
 };
