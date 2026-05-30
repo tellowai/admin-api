88e5f4705d8f07bd9b804a9fb2b258eb7bd255a9
chowdrysai142 <chowdrysai142@gmail.com>
2026-05-30 14:36:31 +0530

    refactor: remove ownerOrAdmin middleware from SQL runner routes
    - Eliminated the ownerOrAdmin permission middleware from the SQL runner routes for listing databases and running queries, simplifying the authorization process.
    - Maintained admin user checks to ensure secure access to SQL operations.

--- modules/sql-runner/routes/sql-runner.route.js
+++ modules/sql-runner/routes/sql-runner.route.js
@@ -2,19 +2,16 @@
 
 const versionConfig = require('../../version');
 const AuthMiddleware = require('../../auth/middlewares/auth.middleware');
-const PermissionMiddleware = require('../../auth/middlewares/permission.middleware');
 const SqlRunnerCtrl = require('../controllers/sql-runner.controller');
 const Validator = require('../validators/sql-runner.validator');
 
 const admin = AuthMiddleware.isAdminUser;
-const ownerOrAdmin = PermissionMiddleware.isOwnerOrAdmin();
 const prefix = versionConfig.routePrefix + '/admin/sql-runner';
 
 module.exports = function (app) {
   app.get(
     prefix + '/databases',
     admin,
-    ownerOrAdmin,
     Validator.validateListDatabases,
     SqlRunnerCtrl.listDatabases,
   );
@@ -22,7 +19,6 @@ module.exports = function (app) {
   app.post(
     prefix + '/query',
     admin,
-    ownerOrAdmin,
     Validator.validateRunQuery,
     SqlRunnerCtrl.runQuery,
   );
